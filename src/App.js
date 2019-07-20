d003348b0a5cba3426aa631b7e0d90cfa36a911b
Farbod Abolhassani <farbod.ab@hotmail.ca>
2019-07-20 11:56:12 -0400

    Add ml functionality

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+
+const app = new Clarifai.App({
+ apiKey: '9ebd1799a7d34d46bd4a268042a16763'
+});
 
 const particlesOptions= {
 	    "particles": {
 	        "number": {
-	            "value": 50
+	            "value": 200
 	        },
 	        "size": {
-	            "value": 5
+	            "value": 2
 	        }
 	    },
     }
@@ -20,6 +27,48 @@ const particlesOptions= {
 
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const face = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return{
+      leftCol: face.left_col * width,
+      topRow: face.top_row * height,
+      rightCol: width - (face.right_col * width),
+      bottomRow: height - (face.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+    console.log(this.state.box)
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value})
+  }
+
+  onSubmit = () => {
+    this.setState({imageUrl: this.state.input })
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+
+  }
+
   render(){
   return (
     <div className="App">
@@ -27,10 +76,8 @@ class App extends React.Component {
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      {/*
-
-      <FaceRecognition />*/}
+      <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit}/>
+      <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
     </div>
   );
 }
